1903b18d532b901cafe9126c6a52e6bb4a4e4119
Ali Basiri <abasiri@netflix.com>
2019-04-04 23:25:44 -0700

    Refactor updateReminders method to be more readable
    in addition:
    * style snooze notification to match Inbox.
    * fix missing reminder text

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-const processedEmails = [];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -8,7 +7,7 @@ let lastEmailCount = 0;
 let lastRefresh = new Date();
 let loadedMenu = false;
 
-const getMyEmail = function () {
+const getMyEmailAddress = function () {
 	const accountInfo = document.querySelector("div[aria-label='Account Information']");
 	if (accountInfo) {
 		for (const child of accountInfo.getElementsByTagName("*")) {
@@ -38,98 +37,87 @@ const waitForElement = function (selector, callback, tries) {
 	}
 };
 
+const isReminder = function(email, myEmailAddress) {
+	const titleNode = email.querySelector(".bqe");
+
+	if (titleNode && titleNode.innerText.toLowerCase().trim() === "reminder") {
+		return true;
+	}
+
+	const nameNodes = email.querySelectorAll(".yP,.zF");
+	let allNamesMe = true;
+
+	for (const nameNode of nameNodes) {
+		if (nameNode.getAttribute("email") !== myEmailAddress) {
+			allNamesMe = false;
+		}
+	}
+
+	return allNamesMe;
+};
+
+const addDateLabel = function (email, label) {
+    if (!email.previousSibling || email.previousSibling.className != "time-row") {
+        const timeRow = document.createElement("div");
+        timeRow.className = "time-row";
+        const time = document.createElement("div");
+        time.className = "time";
+        time.innerText = label;
+        timeRow.appendChild(time);
+
+        email.parentElement.insertBefore(timeRow, email);
+    }
+};
+
+const getDate = function(email) {
+    const dateString = email.querySelector(".xW.xY span").getAttribute("title");
+    return new Date(dateString);
+};
+
+const buildDateLabel = function(email) {
+    let now = new Date();
+    let date = getDate(email);
+
+    if(now.getFullYear() == date.getFullYear()) {
+		if(now.getMonth() == date.getMonth()) {
+			if(now.getDate() == date.getDate()) { return "Today"; }
+			if(now.getDate()-1 == date.getDate()) { return "Yesterday"; }
+			return "This month";
+		}
+    	return months[date.getMonth()];
+    }
+    if(now.getFullYear()-1 == date.getFullYear()) { return "Last year"; }
+
+    return date.getFullYear().toString();
+};
+
+const deleteAllDateLabels =  function() {
+    document.querySelectorAll(".time-row").forEach(row => row.outerHTML = "");
+};
+
 const updateReminders = function () {
 	const emails = document.querySelectorAll(".zA");
-	const myEmail = getMyEmail();
+	const myEmail = getMyEmailAddress();
 	let lastLabel = null;
 	const now = new Date();
 
-	if (emails.length != lastEmailCount || now.getDate() != lastRefresh.getDate()) {
-		document.querySelectorAll(".time-row").forEach(row => row.outerHTML = "");
+	if (emails.length !== lastEmailCount || now.getDate() !== lastRefresh.getDate()) {
+		deleteAllDateLabels();
 		lastEmailCount = emails.length;
 		lastRefresh = now;
 	}
-	
+
 	for (const email of emails) {
-		if (processedEmails.indexOf(email) != -1) continue;
-
-		let isReminder = false;
-
-		const titleNode = email.querySelector(".bqe");
-		if (titleNode && titleNode.innerText.toLowerCase().trim() == "reminder") {
-			isReminder = true;
+		if (isReminder(email, myEmail)) {
+			email.querySelectorAll(".yP,.zF").forEach(node => { node.innerHTML = "Reminder";});
+			email.classList.add("reminder");
 		}
 
-		const nameNodes = email.querySelectorAll(".yP,.zF");
-		let allNamesMe = true;
-
-		for (const nameNode of nameNodes) {
-			if (nameNode.getAttribute("email") != myEmail) {
-				allNamesMe = false;
-			}
-		}
-
-		if (allNamesMe) {
-			for (const nameNode of nameNodes) {
-				nameNode.setAttribute("name", "Reminder");
-				nameNode.setAttribute("email", "reminder");
-				nameNode.innerHTML = "Reminder";
-			}
-
-			isReminder = true;
-		}
-
-		if (isReminder) {
-			email.querySelectorAll(".y6,.Zt").forEach(node => node.outerHTML = "");
-			email.querySelectorAll(".pH.a9q").forEach(node => {
-				node.style.opacity = "1";
-				node.style.backgroundImage = "url('https://i.ibb.co/nRdXnR2/ic-reminder-blue-24dp-r2-2x.png')";
-			});
-			email.querySelectorAll(".y2").forEach(node => node.style.color = "#202124");
-		}
-
-		const dateString = email.querySelector(".xW.xY span").getAttribute("title");
-		const date = new Date(dateString);
-
-		const addLabel = function (label) {
-			if (!email.previousSibling || email.previousSibling.className != "time-row") {
-				const timeRow = document.createElement("div");
-				timeRow.className = "time-row";
-				const time = document.createElement("div");
-				time.className = "time";
-				time.innerText = label;
-				timeRow.appendChild(time);
-
-				email.parentElement.insertBefore(timeRow, email);
-			}
-		};
-
-		let label = "";
-
-		if (now.getFullYear() == date.getFullYear()) {
-			label = months[date.getMonth()];
-
-			if (now.getMonth() == date.getMonth()) {
-				label = "This month";
-
-				if (now.getDate() == date.getDate()) {
-					label = "Today";
-				} else if (now.getDate() - 1 == date.getDate()) {
-					label = "Yesterday";
-				}
-			}
-		} else if (now.getFullYear() - 1 == date.getFullYear()) {
-			label = "Last year";
-		} else {
-			label = date.getFullYear().toString();
-		}
-
-		if (label != lastLabel) {
-			addLabel(label);
+		let label = buildDateLabel(email);
+		if (label !== lastLabel) {
+			addDateLabel(email, label);
 			lastLabel = label;
 		}
-
-		//processedEmails.push(email);
 	}
 
 	setTimeout(updateReminders, 100);
@@ -305,7 +293,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	const addReminder = document.createElement("div");
 	addReminder.className = "add-reminder";
 	addReminder.addEventListener("click", function (e) {
-		const myEmail = getMyEmail();
+		const myEmail = getMyEmailAddress();
 
 		const composeButton = document.querySelector(".T-I.J-J5-Ji.T-I-KE.L3");
 		triggerMouseEvent(composeButton, "mousedown");
