ea920897a08e356a3ce7b3b4cd4cd361f8e64475
Daniel Russell <daniel.russell@c2fo.com>
2019-05-08 22:39:23 +0100

    Update default reminder treatment based on the description
    repo readme and chrome web store description says: 'Display emails sent to yourself with subject "Reminder" as reminders'

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.method === 'getOptions') {
 		const options = JSON.parse(localStorage.getItem('options') || '{}');
-		options.reminderTreatment = options.reminderTreatment || 'all';
+		options.reminderTreatment = options.reminderTreatment || 'containing-word';
 		options.emailBundling = options.emailBundling || 'enabled';
 		options.showAvatar = options.showAvatar || 'enabled';
 
