a26ab272911bdbd1f8e5375bcf31148e5ead8a6e
Daniel Russell <daniel.russell@c2fo.com>
2019-05-08 22:23:15 +0100

    [#76] Extension options as popup on clicking icon

--- src/background.js
+++ src/background.js
@@ -1,12 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.method === "getOptions") {
-		const options = JSON.parse(localStorage.getItem("options") || "{}");
+	if (request.method === 'getOptions') {
+		const options = JSON.parse(localStorage.getItem('options') || '{}');
 		options.reminderTreatment = options.reminderTreatment || 'all';
 		options.emailBundling = options.emailBundling || 'enabled';
 		options.showAvatar = options.showAvatar || 'enabled';
 
 		sendResponse(options);
-	} else {
-		sendResponse({});
-	}
+	} else sendResponse({});
 });
--- src/options/script.js
+++ src/options/script.js
@@ -1,19 +1,19 @@
-const REMINDER_TREATMENT_SELECTOR = "input[name=reminder-treatment]";
-const BUNDLED_EMAIL_SELECTOR = "input[name=email-bundling]";
-const AVATAR_SELECTOR = "input[name=avatar]";
+const REMINDER_TREATMENT_SELECTOR = 'input[name=reminder-treatment]';
+const BUNDLED_EMAIL_SELECTOR = 'input[name=email-bundling]';
+const AVATAR_SELECTOR = 'input[name=avatar]';
 
-function save_options() {
+function saveOptions() {
 	const reminderTreatment = getSelectedRadioValue(REMINDER_TREATMENT_SELECTOR);
 	const emailBundling = getSelectedRadioValue(BUNDLED_EMAIL_SELECTOR);
 	const showAvatar = getSelectedRadioValue(AVATAR_SELECTOR);
 
-	const options = {reminderTreatment, emailBundling, showAvatar};
+	const options = { reminderTreatment, emailBundling, showAvatar };
 
-	localStorage.setItem("options", JSON.stringify(options));
+	localStorage.setItem('options', JSON.stringify(options));
 }
 
-function restore_options() {
-	chrome.runtime.sendMessage({method: "getOptions"}, function(options) {
+function restoreOptions() {
+	chrome.runtime.sendMessage({ method: 'getOptions' }, function(options) {
 		selectRadioWithValue(REMINDER_TREATMENT_SELECTOR, options.reminderTreatment);
 		selectRadioWithValue(BUNDLED_EMAIL_SELECTOR, options.emailBundling);
 		selectRadioWithValue(AVATAR_SELECTOR, options.showAvatar);
@@ -26,15 +26,11 @@ function selectRadioWithValue(selector, value) {
 	});
 }
 
-function getSelectedRadioValue(selector) {
-	return document.querySelector(selector + ":checked").value;
-}
+const getSelectedRadioValue = selector => document.querySelector(selector + ':checked').value;
 
-function monitorChange(element) {
-	element.addEventListener('click', save_options);
-}
+const monitorChange = element => element.addEventListener('click', saveOptions);
 
-document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelectorAll(REMINDER_TREATMENT_SELECTOR).forEach(monitorChange);
 document.querySelectorAll(BUNDLED_EMAIL_SELECTOR).forEach(monitorChange);
 document.querySelectorAll(AVATAR_SELECTOR).forEach(monitorChange);
